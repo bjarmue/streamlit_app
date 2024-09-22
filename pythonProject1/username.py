5b39176fdfbe067cf53027e07c8703cfb1ac8876
bjarmue <bjarne.mueller.2006@gmail.com>
2024-09-22 15:33:58 +0200

    Update username.py

--- pythonProject1/username.py
+++ pythonProject1/username.py
@@ -2,11 +2,12 @@ import streamlit as st
 import pandas as pd
 from streamlit_gsheets import GSheetsConnection
 def username():
+    url = "https://docs.google.com/spreadsheets/d/1g4dlVCqSFRCw6rZeJLzirW_tL50wdQGLGDmov5T_4u4/edit?gid=0#gid=0"
     #Zugriff auf Nummer des User durch Login
     number=st.session_state["number"]
 
     #Zugriff auf Sheets
-    conn= st.connection("gsheets",type=GSheetsConnection)
+    conn= st.connection(spreadsheet=url,type=GSheetsConnection)
 
     #Zugriff auf den Namen des User in Abhängigkeit seiner Nummer
     personal_name= conn.read(worksheet="Tabellenblatt1",usecols=[0,1],ttl=1)
